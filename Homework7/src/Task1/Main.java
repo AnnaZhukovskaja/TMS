f790283c56a592c982a62b3e492996cafad15794
AnnaZhukovskaja <kulnis.anna@gmail.com>
2023-12-04 12:18:25 +0300

    homework 7-1 fixed

--- Homework7/src/Task1/Main.java
+++ Homework7/src/Task1/Main.java
@@ -11,10 +11,19 @@ public class Main {
 
         Shapes[] shapes = new Shapes[5];
         shapes[0] = new Circle(5.0);
+        System.out.println("The area of the Circle 1 = " + shapes[0].area());
+
         shapes[1] = new Circle(8.0);
+        System.out.println("The area of the Circle 2 = " + shapes[1].area());
+
         shapes[2] = new Rectangle(3, 5);
+        System.out.println("The area of the Rectangle 1 = " + shapes[2].area());
+
         shapes[3] = new Rectangle(1, 3);
+        System.out.println("The area of the Rectangle 2 = " + shapes[3].area());
+
         shapes[4] = new Triangle(3, 4, 5, 6);
+        System.out.println("The area of the Triangle = " + shapes[3].area());
 
         System.out.println("Sum of the perimeters of all shapes =  " + (shapes[0].perimeter() + shapes[1].perimeter() + shapes[2].perimeter() + shapes[3].perimeter() + shapes[4].perimeter()));
 
